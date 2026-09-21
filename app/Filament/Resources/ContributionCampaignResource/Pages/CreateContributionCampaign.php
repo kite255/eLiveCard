62bd25d32511dc825089b0edc0ccd2e0bcf99941
kite255 <160236713+kite255@users.noreply.github.com>
2026-09-21 16:23:58 +0300

    feat: open contribution designer after campaign creation

--- app/Filament/Resources/ContributionCampaignResource/Pages/CreateContributionCampaign.php
+++ app/Filament/Resources/ContributionCampaignResource/Pages/CreateContributionCampaign.php
@@ -8,4 +8,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateContributionCampaign extends CreateRecord
 {
     protected static string $resource = ContributionCampaignResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return ContributionCampaignResource::getUrl('designer', [
+            'record' => $this->record,
+        ]);
+    }
 }
